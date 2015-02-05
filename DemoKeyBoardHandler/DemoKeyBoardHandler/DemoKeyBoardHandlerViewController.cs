2fc90aa39a9d87ea96af15ed319f4ebab5d4d0fa
Tuan Vu <vdtuan.cse10@gmail.com>
2015-02-05 21:52:12 +0700

    Add KeyboadHandler

---
+++ DemoKeyBoardHandler/DemoKeyBoardHandler/DemoKeyBoardHandlerViewController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace DemoKeyBoardHandler
+{
+	public partial class DemoKeyBoardHandlerViewController : UIViewController
+	{
+		private TVKeyboardHandler _tvKeyboardHandler;
+		private NSObject _keyboardUp;
+		private NSObject _keyboardDown;
+
+		public DemoKeyBoardHandlerViewController (IntPtr handle) : base (handle)
+		{
+			_tvKeyboardHandler = new TVKeyboardHandler ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+			_tvKeyboardHandler.View = this.View;
+			_keyboardUp = NSNotificationCenter
+				.DefaultCenter
+				.AddObserver(UIKeyboard.DidShowNotification,
+					_tvKeyboardHandler.KeyboardUpNotification);
+
+			_keyboardDown = NSNotificationCenter
+				.DefaultCenter
+				.AddObserver(UIKeyboard.WillHideNotification,
+					_tvKeyboardHandler.KeyboardDownNotification);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+			if (_keyboardUp != null && _keyboardDown != null)
+			{
+				NSNotificationCenter.DefaultCenter.RemoveObserver(_keyboardUp);
+				NSNotificationCenter.DefaultCenter.RemoveObserver(_keyboardDown);
+			}
+		}
+
+		#endregion
+
+
+		public override void TouchesBegan (NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan (touches, evt);
+			View.EndEditing (true);
+		}
+	}
+}
+
---
+++ DemoKeyBoardHandler/DemoKeyBoardHandler/TVKeyboardHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using System.Drawing;
+using CoreGraphics;
+
+namespace DemoKeyBoardHandler
+{
+	public class TVKeyboardHandler
+	{
+		private UIView _activeView; // Controller that activated the keyboard
+		public UIView View { get; set; } // The UIView for the keyboard handler
+
+		private nfloat _scrollAmount = 0; // amount to scroll 
+		private nfloat _scrolledAmount = 0; // how much we've
+		// scrolled already
+		private nfloat _bottom = 0.0f; // bottom point
+		private const float OFFSET = 10.0f; // extra offset 
+
+		public void KeyboardUpNotification(NSNotification notification)
+		{
+			// get the keyboard size
+			var val = (NSValue)notification.UserInfo.ValueForKey (UIKeyboard.FrameBeginUserInfoKey);
+			RectangleF keyboardFrame = val.RectangleFValue;
+
+			// Find what opened the keyboard
+			foreach (UIView view in this.View.Subviews)
+			{
+				if (view.IsFirstResponder)
+					_activeView = view;
+			}
+			if (_activeView != null) {
+				// Determine if we need to scroll up or down.
+				// Bottom of the controller = initial position + height + offset 
+				_bottom = (_activeView.Frame.Y + _activeView.Frame.Height + OFFSET);
+				// Calculate how far we need to scroll
+				_scrollAmount = (keyboardFrame.Height - (View.Frame.Size.Height - _bottom));
+
+				//Move view up
+				if (_scrollAmount > 0)
+				{
+					//Subtract the scrolledamount. We can’t do this subtraction above because the calculations won’t work correctly.
+					_bottom -= _scrolledAmount;
+					_scrollAmount = (keyboardFrame.Height - (View.Frame.Size.Height - _bottom));
+					_scrolledAmount += _scrollAmount;
+					ScrollTheView(false);
+				}
+				//Reset the view.
+				else
+				{
+					ScrollTheView(true);
+				}
+			}
+		}
+		public void KeyboardDownNotification(NSNotification
+			notification)
+		{
+			ScrollTheView(true);
+		}
+
+		private void ScrollTheView(bool reset)
+		{
+			// scroll the view up or down
+			UIView.BeginAnimations(string.Empty, System.IntPtr.Zero);
+			UIView.SetAnimationDuration(0.3);
+			CGRect frame = View.Frame;
+			if (reset)
+			{
+				frame.Y = frame.Y + _scrolledAmount;
+				_scrollAmount = 0;
+				_scrolledAmount = 0;
+			}
+			else
+			{
+				frame.Y -= _scrollAmount;
+			}
+			View.Frame = frame;
+			UIView.CommitAnimations();
+		}
+	}
+}
+
